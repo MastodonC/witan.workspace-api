5e1bf0287d9a96efae1083bcef1054af50bb6e62
Antony Woods <antony@mastodonc.com>
2016-05-25 13:09:48 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject witan.workspace-api "0.1.0-SNAPSHOT"
+(defproject witan.workspace-api "0.1.0"
   :description "A helper library for writing models in the style of Witan workspaces."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
