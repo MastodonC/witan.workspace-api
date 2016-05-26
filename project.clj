d1dda125d88c7b038749cf7f83c0abba762a6535
Antony Woods <antony@mastodonc.com>
2016-05-26 10:17:23 +0100

    Add schema hashes

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.51.0"]
-                 [prismatic/schema "1.1.0"]])
+                 [prismatic/schema "1.1.0"]
+                 [pandect "0.6.0"]])
