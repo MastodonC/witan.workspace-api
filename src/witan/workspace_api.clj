37c797500a54a365378998375e95ea5e2bd0f237
Antony Woods <antony@mastodonc.com>
2016-05-27 10:00:29 +0100

    Add predicate support

--- src/witan/workspace_api.clj
+++ src/witan/workspace_api.clj
@@ -13,12 +13,23 @@
         result (if-not (-> schema keys has-any?) (select-keys m (keys schema)) m)]
     (s/validate schema' result)))
 
+
+
 (def WorkflowFnMetaData
   "Schema for the Witan workflow function metadata"
   {:witan/name          s/Keyword
    :witan/version       s/Str
    :witan/input-schema  {s/Keyword s/Any}
    :witan/output-schema {s/Keyword s/Any}
+   :witan/doc           s/Str
+   (s/optional-key :witan/param-schema) {s/Any s/Any}
+   (s/optional-key :witan/exported?) s/Bool})
+
+(def WorkflowPredicateMetaData
+  "Schema for the Witan workflow predicate metadata"
+  {:witan/name          s/Keyword
+   :witan/version       s/Str
+   :witan/input-schema  {s/Keyword s/Any}
    :witan/doc           s/Str
    (s/optional-key :witan/param-schema) {s/Any s/Any}
    (s/optional-key :witan/exported?) s/Bool})
@@ -32,13 +43,15 @@
 (def WorkflowStatement
   [(s/one s/Keyword "ingress") (s/one (s/if keyword?
                                         s/Keyword
-                                        [(s/one s/Any "predicate") 
+                                        [(s/one s/Any "predicate")
                                          (s/one s/Keyword "predicate-true")
                                          (s/one s/Keyword "predicate-false")]) "egress")])
 
 (def WorkflowModel
   "Schema for the Witan workflow model metadata"
   [WorkflowStatement])
+
+
 
 (defmacro defworkflowfn
   "Macro for defining a workflow function"
@@ -59,13 +72,36 @@
                      (s/validate WorkflowFnMetaData metadata)))
        ~doc
        [inputs# & params#]
-       (let [params'# (when (and (first params#) ~param-schema)
+       (let [params'# (when ~param-schema
                         (select-schema-keys ~param-schema (first params#)))
              inputs'# (select-schema-keys ~input-schema inputs#)
              result#  ((fn ~args ~@body) inputs'# params'#)
              result'# (select-schema-keys ~output-schema result#)]
          (merge inputs# result'#)))))
 
+(defmacro defworkflowpred
+  "Macro for defining a workflow predicate"
+  [name & body] ;; metadata args &body
+  (let [doc      (when (string? (first body)) (first body))
+        metadata (if doc (second body) (first body))
+        body     (if doc (drop 2 body) (next body))
+        args     (first body)
+        body     (next body)
+        doc      (or doc "No docs")
+        metadata (assoc metadata :witan/doc doc)
+        {:keys [witan/input-schema
+                witan/param-schema]} metadata]
+    `(defn ~(with-meta name
+              (assoc (meta name)
+                     :witan/workflowpred
+                     (s/validate WorkflowPredicateMetaData metadata)))
+       ~doc
+       [inputs# & params#]
+       (let [params'# (when ~param-schema
+                        (select-schema-keys ~param-schema (first params#)))
+             inputs'# (select-schema-keys ~input-schema inputs#)
+             result#  ((fn ~args ~@body) inputs'# params'#)]
+         (boolean result#)))))
 
 (defmacro defworkflowmodel
   "Macro for defining a workflow model"
@@ -76,11 +112,13 @@
         doc      (or doc "No docs")
         metadata (assoc metadata :witan/doc doc)]
     `(def ~(with-meta name
-              (assoc (meta name)
-                     :witan/workflowmodel
-                     (s/validate WorkflowModelMetaData metadata)))
+             (assoc (meta name)
+                    :witan/workflowmodel
+                    (s/validate WorkflowModelMetaData metadata)))
        ~doc
        ~@body)))
+
+
 
 (defmacro merge->
   [data & forms]
