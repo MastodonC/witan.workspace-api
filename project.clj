7ff66d200777c52e95fbb0480f99cd664bca13fc
Antony Woods <antony@mastodonc.com>
2016-06-01 15:39:58 +0100

    Fixed URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject witan.workspace-api "0.1.2-SNAPSHOT"
   :description "A helper library for writing models in the style of Witan workspaces."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/MastodonC/witan.workspace-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
