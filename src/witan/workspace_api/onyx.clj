f507b1018a9d5a2e8b053868cb4f220710122098
Antony Woods <antony@mastodonc.com>
2016-07-04 17:12:57 +0100

    Add default pred wrapper and rename fn wrapper

--- src/witan/workspace_api/onyx.clj
+++ src/witan/workspace_api/onyx.clj
@@ -10,7 +10,7 @@
     (catch Throwable e
       (throw (ex-info (str "Could not resolve symbol on the classpath, did you require the file that contains the symbol " kw "?") {:kw kw})))))
 
-(defn default-wrapper
+(defn default-fn-wrapper
   "This function exists to act as a 'wrapper' for when workflow functions are to
    be used in an Onyx job, the original reason being that workflow functions expect
    [state params] whereas Onyx functions expect [params state]. Unfortunately we
@@ -18,4 +18,12 @@
   ([fn segment]
    ((kw->fn fn) segment))
   ([fn params segment]
+   ((kw->fn fn) segment params)))
+
+(defn default-pred-wrapper
+  "This function exists to act as a 'wrapper' for when workflow predicates are to
+   be used in an Onyx job"
+  ([_ _ segment _ fn]
+   ((kw->fn fn) segment))
+  ([_ _ segment _ fn params]
    ((kw->fn fn) segment params)))
