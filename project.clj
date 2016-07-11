f9bf62f779639fb2900c8a30c137ba770f495ce2
Antony Woods <antony@mastodonc.com>
2016-07-11 10:15:44 +0100

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject witan.workspace-api "0.1.6-SNAPSHOT"
+(defproject witan.workspace-api "0.1.7-SNAPSHOT"
   :description "A helper library for writing models in the style of Witan workspaces."
   :url "https://github.com/MastodonC/witan.workspace-api"
   :license {:name "Eclipse Public License"
