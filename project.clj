ad7a6fa2759c8758900beb49b0577910d55af945
Eleonore9 <eleonore.mayola@ymail.com>
2016-08-01 18:45:57 +0100

    Wrappers around Incanter functions

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.51.0"]
-                 [prismatic/schema "1.1.2"]]
+                 [prismatic/schema "1.1.2"]
+                 [kixi/incanter-core "1.9.1-p0-3bf644a"]]
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]
                  ["snapshots" {:url "https://clojars.org/repo"
--- test/witan/datasets_test.clj
+++ test/witan/datasets_test.clj
@@ -3,7 +3,7 @@
             [clojure.core.matrix.dataset :as ds]
             [clojure.test :refer :all]))
 
-(def data 
+(def data
   (ds/dataset [:label :label2 :value]
               (mapv conj
                     (cycle (for [f [:a :b :c]
@@ -13,44 +13,44 @@
 
 (def results
   {:max {[] (ds/dataset [:value] [[100000]])
-         [:label] (ds/dataset [:label :value] 
+         [:label] (ds/dataset [:label :value]
                               [[:a 100000] [:b 99995] [:c 99998]])
-         [:label :label2] (ds/dataset [:label :label2 :value] 
+         [:label :label2] (ds/dataset [:label :label2 :value]
                                       [[:a :x 99999] [:a :y 100000] [:a :z 99992]
                                        [:b :x 99993] [:b :y 99994] [:b :z 99995]
                                        [:c :x 99996] [:c :y 99997] [:c :z 99998]])}
    :min {[] (ds/dataset [:value] [[0]])
-         [:label] (ds/dataset [:label :value] 
+         [:label] (ds/dataset [:label :value]
                               [[:a 0] [:b 3] [:c 6]])
-         [:label :label2] (ds/dataset [:label :label2 :value] 
+         [:label :label2] (ds/dataset [:label :label2 :value]
                                       [[:a :x 0] [:a :y 1] [:a :z 2]
                                        [:b :x 3] [:b :y 4] [:b :z 5]
                                        [:c :x 6] [:c :y 7] [:c :z 8]])}
    :sum {[] (ds/dataset [:value] [[5000050000]])
-         [:label] (ds/dataset [:label :value] 
+         [:label] (ds/dataset [:label :value]
                               [[:a 1666716667] [:b 1666616667] [:c 1666716666]])
-         [:label :label2] (ds/dataset [:label :label2 :value] 
+         [:label :label2] (ds/dataset [:label :label2 :value]
                                       [[:a :x 555594444] [:a :y 555605556] [:a :z 555516667]
                                        [:b :x 555527778] [:b :y 555538889] [:b :z 555550000]
                                        [:c :x 555561111] [:c :y 555572222] [:c :z 555583333]])}
    :count {[] (ds/dataset [:value] [[100001]])
-           [:label] (ds/dataset [:label :value] 
+           [:label] (ds/dataset [:label :value]
                                 [[:a 33335] [:b 33333] [:c 33333]])
-           [:label :label2] (ds/dataset [:label :label2 :value] 
+           [:label :label2] (ds/dataset [:label :label2 :value]
                                         [[:a :x 11112] [:a :y 11112] [:a :z 11111]
                                          [:b :x 11111] [:b :y 11111] [:b :z 11111]
                                          [:c :x 11111] [:c :y 11111] [:c :z 11111]])}
    :mean {[] (ds/dataset [:value] [[50000]])
-          [:label] (ds/dataset [:label :value] 
+          [:label] (ds/dataset [:label :value]
                                [[:a (/ 1666716667 33335)] [:b 49999] [:c 50002]])
-          [:label :label2] (ds/dataset [:label :label2 :value] 
+          [:label :label2] (ds/dataset [:label :label2 :value]
                                        [[:a :x (/ 555594444 11112)] [:a :y (/ 555605556 11112)] [:a :z (/ 555516667 11111)]
                                         [:b :x (/ 555527778 11111)] [:b :y (/ 555538889 11111)] [:b :z (/ 555550000 11111)]
                                         [:c :x (/ 555561111 11111)] [:c :y (/ 555572222 11111)] [:c :z (/ 555583333 11111)]])}})
 
 (deftest rollup
   (doseq [aggregate [:max :min :sum :count :mean]
-          groupby [[] [:label] [:label :label2]]]    
+          groupby [[] [:label] [:label :label2]]]
     (is
      (=
       (get-in results [aggregate groupby])
@@ -61,7 +61,7 @@
 
 (def test-data
   (map zipmap
-       (repeat [:a :b :c]) 
+       (repeat [:a :b :c])
        (map #(vector % % %) (range data-size))))
 
 (defn d
@@ -146,11 +146,11 @@
           (ds/column-names joined))))))
 
 (deftest filter-dataset-test
-  (let [filter-dataset-fn (fn [a] 
+  (let [filter-dataset-fn (fn [a]
                             (<= (/ data-size 2)
                                 a))
-        filter-maps-fn (fn [m] 
-                         (<= (/ data-size 2) 
+        filter-maps-fn (fn [m]
+                         (<= (/ data-size 2)
                              (:a m)))
         filtered (wds/filter-dataset (ds/dataset test-data) [:a] filter-dataset-fn)]
     (is
@@ -160,3 +160,41 @@
      (= (ds/dataset
          (filter filter-maps-fn test-data))
         filtered))))
+
+(def test-ds (ds/dataset [{:a 1 :b 2 :c 3}
+                          {:a 4 :b 5 :c 6}
+                          {:a 7 :b 8 :c 9}]))
+
+(deftest select-from-ds-test
+  (testing "The function works as the Incanter equivalent."
+    (is (= (ds/dataset [{:a 7 :b 8 :c 9}])
+           (wds/select-from-ds test-ds
+                               {:a {:gte 4} :b {:gt 5}})))))
+
+(deftest subset-ds-test
+  (testing "The function works as the Incanter equivalent."
+    (is (= 4
+           (wds/subset-ds test-ds :rows 1 :cols :a)))))
+
+(deftest group-ds-test
+  (testing "The function works as the Incanter equivalent."
+    (is (= {{:a 1 :b 2}
+            (ds/dataset [{:a 1 :b 2 :c 3}])
+            {:a 4 :b 5}
+            (ds/dataset [{:a 4 :b 5 :c 6}])
+            {:a 7 :b 8}
+            (ds/dataset [{:a 7 :b 8 :c 9}])}
+           (wds/group-ds test-ds [:a :b])))))
+
+(defn- fp-equals? [x y ε] (< (Math/abs (- x y)) ε))
+
+(deftest linear-model-test
+  (testing "The function works as the Incanter equivalent."
+    (let [test-y [1 2 3 4 5 6 7 8 9]
+          test-x [2 4 6 8 10 12 14 16 18]]
+      (is (fp-equals? 0.9999999999999999
+                      (:r-square (wds/linear-model test-y test-x)) 0.00000000001))
+      (is (every? true?
+                  (map #(fp-equals? %1 %2 0.00000000001)
+                       '(-3.552713678800501E-15 0.4999999999999998)
+                       (:coefs (wds/linear-model test-y test-x :intercept 1))))))))
