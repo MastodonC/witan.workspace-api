71c768b5e4b1c6ac862fdfa65dbe53f3f2189aa3
Antony Woods <antony@mastodonc.com>
2016-08-01 16:47:13 +0100

    Fix the meta macros

--- src/witan/workspace_api/utils.clj
+++ src/witan/workspace_api/utils.clj
@@ -1,6 +1,15 @@
 (ns witan.workspace-api.utils)
 
-(defmacro map-meta
+(defn assert!
+  [p msg]
+  (when-not (p) (throw (Exception. msg))))
+
+(defmacro map-fn-meta
   [& ss]
   `[~@(for [s ss]
         `(-> #'~s meta :witan/metadata))])
+
+(defmacro map-model-meta
+  [& ss]
+  `[~@(for [s ss]
+        `(assoc ~s :metadata (-> #'~s meta :witan/metadata)))])
